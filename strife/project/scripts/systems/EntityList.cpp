fd42d0e56a5a7f1162887181bd1366053dfda30e
Michael Schroder <thormme@yahoo.com>
2018-10-13 17:18:23 -0700

    EntityList Fields now editable

--- strife/project/scripts/systems/EntityList.cpp
+++ strife/project/scripts/systems/EntityList.cpp
@@ -7,6 +7,7 @@
 #include "utility/imgui_sdl.h"
 #include "Scene.h"
 #include "Dispatcher.h"
+#include "Data.h"
 #include "events/RenderEvent.h"
 
 using namespace Strife::Core;
@@ -21,6 +22,72 @@ EntityList::EntityList(Strife::Core::Scene& scene, Strife::Core::Dispatcher& dis
 }
 
 EntityList::~EntityList() {}
+
+Data renderData(Data item, string key = "") {
+    switch (item.type()) {
+        case Data::value_t::object: {
+            for (auto pair : item.items()) {
+                item[pair.key()] = renderData(pair.value(), pair.key());
+            }
+            break;
+        }
+        case Data::value_t::number_integer: {
+            int value = static_cast<int>(item.get<Data::number_integer_t>());
+            if (ImGui::InputInt(key.c_str(), &value)) {
+                *item.get<Data::number_integer_t*>() = value;
+            }
+            break;
+        }
+        case Data::value_t::number_unsigned: {
+            int value = static_cast<int>(item.get<Data::number_unsigned_t>());
+            if (ImGui::InputInt(key.c_str(), &value)) {
+                if (value >= 0) {
+                    *item.get<Data::number_integer_t*>() = value;
+                }
+            }
+            break;
+        }
+        case Data::value_t::number_float: {
+            double* value = item.get<Data::number_float_t*>();
+            if (ImGui::InputDouble(key.c_str(), value)) {
+                //*item.value().get<Data::number_integer_t*>() = value;
+            }
+            break;
+        }
+        case Data::value_t::string: {
+
+            string value = item.get<Data::string_t>();
+
+            std::string clipText = (ImGui::GetClipboardText() != nullptr) ? ImGui::GetClipboardText() : "";
+            size_t clipSize = clipText.length();
+            size_t size = value.length() + clipSize + 5;
+            char* newText = static_cast<char*>(malloc(sizeof(char) * size));
+            strncpy(newText, value.c_str(), size);
+
+            if (ImGui::InputText(key.c_str(), newText, size)) {
+                item = Data(string(newText));
+            }
+            delete newText;
+            break;
+        }
+        case Data::value_t::boolean:
+        case Data::value_t::null:
+        case Data::value_t::array: {
+            size_t index = 0;
+            for (auto arrayItem : item) {
+                stringstream indexString;
+                indexString << index;
+                item[index] = renderData(arrayItem, indexString.str());
+                index++;
+            }
+            break;
+        }
+        default:
+            ImGui::Text("%s", key.c_str());
+            ImGui::Text("%s", item.dump().c_str());
+    }
+    return item;
+}
 
 void EntityList::render(const RenderEvent& event) {
     size_t numEntities = scene_.entities.get().size();
@@ -53,10 +120,11 @@ void EntityList::render(const RenderEvent& event) {
                     auto component = storage->get(entity);
                     if (component != nullptr) {
                         if (ImGui::TreeNode(scene_.components.identifier(type).c_str())) {
-                            string componentData = component->serialize().dump();
-                            ImGui::Text("%s", componentData.c_str());
+                            Data componentData = component->serialize();
+                            Data modifiedData = renderData(componentData);
+                            EntityMap emap(scene_);  // TODO: Need to add option to avoid reassigning uuids
+                            component->deserialize(modifiedData, emap);
                             ImGui::TreePop();
-                            ImGui::Separator();
                         }
                     }
                 }
